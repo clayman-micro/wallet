53767b3636dfdc1f78e5b3dd63e83ab544d4dbe0
Kirill Sumorokov <sumorokov.k@gmail.com>
2017-12-09 18:00:15 +0500

    Refactor validator

---
+++ tests/test_validator.py
@@ -0,0 +1,68 @@
+from datetime import datetime
+
+import pytest
+
+from wallet.entities import OperationType
+from wallet.validation import ValidationError, Validator
+
+
+@pytest.mark.clean
+@pytest.mark.parametrize('payload', ('True', 'true', '1', 'yes'))
+def test_validator_coerce_boolean(payload):
+    schema = {'enabled': {'type': 'boolean', 'coerce': 'bool'}}
+
+    validator = Validator(schema)
+    document = validator.validate_payload({'enabled': payload})
+
+    assert isinstance(document['enabled'], bool)
+    assert document['enabled']
+
+
+@pytest.mark.clean
+def test_validator_coerce_datetime():
+    schema = {'created_on': {'type': 'datetime', 'coerce': 'datetime'}}
+
+    validator = Validator(schema)
+    document = validator.validate_payload({'created_on': '2017-12-01T23:23:23'})
+
+    assert isinstance(document['created_on'], datetime)
+
+
+@pytest.mark.clean
+@pytest.mark.parametrize('payload', ({}, {'foo': 'bar'}))
+def test_validator_utcnow_default_setter(payload):
+    validator = Validator({'created_on': {
+        'type': 'datetime',
+        'coerce': 'datetime',
+        'default_setter': 'utcnow',
+        # 'empty': True
+    }})
+    document = validator.validate_payload(payload, update=True)
+
+    print(document)
+    assert 'created_on' in document
+    assert isinstance(document['created_on'], datetime)
+
+
+@pytest.mark.clean
+@pytest.mark.parametrize('payload,expected', (
+    ('expense', OperationType.EXPENSE),
+    ('income', OperationType.INCOME)
+))
+def test_validator_coerce_operation_type(payload, expected):
+    schema = {'type': {'type': 'operation_type', 'coerce': 'operation_type'}}
+
+    validator = Validator(schema)
+    document = validator.validate_payload({'type': payload})
+
+    assert document['type'] == expected
+
+
+@pytest.mark.clean
+@pytest.mark.parametrize('payload', ('transfer', 'unknown'))
+def test_validator_coerce_unknown_operation_type(payload):
+    schema = {'type': {'type': 'operation_type', 'coerce': 'operation_type'}}
+
+    with pytest.raises(ValidationError):
+        validator = Validator(schema)
+        validator.validate_payload({'type': payload})
--- wallet/validation.py
+++ wallet/validation.py
@@ -14,29 +14,6 @@ class ValidationError(Exception):
         return self._errors
 
 
-class Validator(cerberus.Validator):
-    def _validate_type_decimal(self, field, value):
-        if not isinstance(value, Decimal):
-            self._error(field, cerberus.errors.ERROR_BAD_TYPE.format('Decimal'))
-
-    def validate_payload(self, payload: Dict, update: bool=False) -> Dict:
-        if not self.validate(payload, update=update):
-            raise ValidationError(self.errors)
-
-        return self.document
-
-
-def to_bool(value):
-    if isinstance(value, bool):
-        return value
-    else:
-        return str(value).lower() in ['true', '1', 'yes']
-
-
-def to_datetime(value: str) -> datetime:
-    return datetime.strptime(value, '%Y-%m-%dT%H:%M:%S')
-
-
 def to_decimal(precision: int) -> Callable:
     quant = ''.join(('0.', ''.join(map(lambda x: '0',
                                        range(precision - 1))), '1'))
@@ -48,3 +25,38 @@ def to_decimal(precision: int) -> Callable:
         except ValueError:
             return None
     return wrapped
+
+
+class Validator(cerberus.Validator):
+    def __init__(self, *args, precision: int=2, **kwargs) -> None:
+        super(Validator, self).__init__(*args, **kwargs)
+        self.precision = precision
+
+    def _validate_type_decimal(self, value):
+        return isinstance(value, Decimal)
+
+    def _normalize_coerce_bool(self, value):
+        if isinstance(value, bool):
+            return value
+        else:
+            return str(value).lower() in ['true', '1', 'yes']
+
+    def _normalize_coerce_datetime(self, value):
+        if value:
+            if isinstance(value, datetime):
+                return value
+            else:
+                return datetime.strptime(value, '%Y-%m-%dT%H:%M:%S')
+
+    def _normalize_coerce_decimal(self, value):
+        return to_decimal(self.precision)(value)
+
+    def _normalize_default_setter_utcnow(self, document):
+        return datetime.utcnow()
+
+    def validate_payload(self, payload: Dict, update: bool=False) -> Dict:
+        self.allow_unknown = update
+        if not self.validate(payload, update=update):
+            raise ValidationError(self.errors)
+
+        return self.document
