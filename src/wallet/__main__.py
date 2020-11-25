4c8513afbaf52b3fbc9a592c866b19833a5a1e2d
Kirill Sumorokov <sumorokov.k@gmail.com>
2020-11-25 19:48:11 +0000

    Remove legacy code

--- src/wallet/__main__.py
+++ src/wallet/__main__.py
@@ -1,5 +1,4 @@
 import asyncio
-import os
 
 import click
 import uvloop  # type: ignore
@@ -7,24 +6,18 @@ from aiohttp_micro.management.server import server  # type: ignore
 from config import (  # type: ignore
     ConsulConfig,
     EnvValueProvider,
-    FileValueProvider,
     load,
 )
-
 
 from wallet.app import AppConfig, init
 
 
 @click.group()
-@click.option("--conf-dir", default=None)
-@click.option('--debug', is_flag=True, default=False)
+@click.option("--debug", is_flag=True, default=False)
 @click.pass_context
-def cli(ctx, conf_dir: str = None, debug: bool = False) -> None:
+def cli(ctx, debug: bool = False) -> None:
     uvloop.install()
     loop = asyncio.get_event_loop()
-
-    if not conf_dir:
-        conf_dir = os.path.dirname(__file__)
 
     consul_config = ConsulConfig()
     load(consul_config, providers=[EnvValueProvider()])
@@ -36,17 +29,17 @@ def cli(ctx, conf_dir: str = None, debug: bool = False) -> None:
             "db": {
                 "user": "wallet",
                 "password": "wallet",
-                "database": "wallet"
+                "database": "wallet",
             },
         }
     )
-    load(config, providers=[FileValueProvider(conf_dir), EnvValueProvider()])
+    load(config, providers=[EnvValueProvider()])
 
     app = init("wallet", config)
 
-    ctx.obj['app'] = app
-    ctx.obj['config'] = config
-    ctx.obj['loop'] = loop
+    ctx.obj["app"] = app
+    ctx.obj["config"] = config
+    ctx.obj["loop"] = loop
 
 
 cli.add_command(server, name="server")
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,99 +1,61 @@
-import os
-import subprocess
-from pathlib import Path
 from typing import Any
 
 import faker  # type: ignore
 import pendulum  # type: ignore
 import pytest  # type: ignore
 from aiohttp import web
+from aiohttp_storage.tests import storage  # type: ignore
 from passport.domain import User  # type: ignore
 
 from wallet.app import AppConfig, init
 
 
 @pytest.fixture(scope="session")
+def fake():
+    return faker.Faker()
+
+
+@pytest.fixture(scope="function")
+def user(fake: Any) -> User:
+    return User(key=1, email=fake.free_email())
+
+
+@pytest.fixture(scope="session")
 def config():
     conf = AppConfig()
-
-    conf.tokens.public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDBqkP6h/7LMOAaWuVAqjPcrOdUCiMhyykbMaDji8odBKtCam1MyBxq1I87LFiAbHx7r5biBMUC/nyTzPiYF5II+g4MDLgV5S8/6uTmtCL40FsuIOClFPqbAiitvRFYDuBTJx3w1Fr4zWWIVtaUFqer5nAsnr4sovOG+zRVtfXJ8w=="  # noqa
-    conf.tokens.private_key = """
------BEGIN RSA PRIVATE KEY-----
-MIICXgIBAAKBgQDBqkP6h/7LMOAaWuVAqjPcrOdUCiMhyykbMaDji8odBKtCam1M
-yBxq1I87LFiAbHx7r5biBMUC/nyTzPiYF5II+g4MDLgV5S8/6uTmtCL40FsuIOCl
-FPqbAiitvRFYDuBTJx3w1Fr4zWWIVtaUFqer5nAsnr4sovOG+zRVtfXJ8wIDAQAB
-AoGBAKiSIQuwVmrdByRJnCU2QWBDLDQtgrkGkqg2AZou8mVhzARKiQr9YCbpECds
-iTh3tb8fbtEbX7UkeKFaF8SjN5uPCIX6MVoQDoH8VTmsj6lFIXpeCjfqEXEFdE/p
-AAJVGZBxN0VNmpVYpp1XhGQE4iI7WBUZtJ7stP0WcjkXkzsRAkEA4h9aGY7bbHru
-lX7PgguP+FMMcOSc+Ax7oQuBZ0x0zXO1YVE1vl4KHeDSuATI6bT4LYGJFa3soUnF
-PMFi6MacGwJBANtBCP1h7LdOFWRtVpOCNCjNYGeuRzxJ3APiolnMpTsUQ8qDWa+M
-UrcO0WhUy7Z+swicEDuKO0CoOjkOQFhZtwkCQQCTFFmCrk1DLmLpkmZe7C5lE3/Q
-HqOLJHN1uQoeqrh+uniMKEqQ3JIwBQCK+XHFshSLZOpJ06tK7bUBY7h2OFlpAkBK
-EOcziXAI4ETTvyfe/r4WBoMJo1MHJ8A+Q8IqabprgcYA1GxopBORKV1OTE7g4F4k
-i2vkYSbxCaNZgNn1vqDZAkEAvBFt/ERW1UnV6fwOTa9x6yxNVNpd3tKRD4li+u0x
-oAvGVHdn+B1JJBkTJccu9hOAWjyXX5C2QuuC/fNKmsqxyQ==
------END RSA PRIVATE KEY-----
-    """
 
     return conf
 
 
 @pytest.yield_fixture(scope="function")
-def app(loop, pg_server, config):
+def app(pg_server, config):
     config.db.host = pg_server["params"]["host"]
     config.db.port = pg_server["params"]["port"]
     config.db.user = pg_server["params"]["user"]
     config.db.password = pg_server["params"]["password"]
     config.db.database = pg_server["params"]["database"]
 
-    app = loop.run_until_complete(init("wallet", config))
+    app = init("wallet", config)
+
+    with storage(config=app["config"].db, root=app["storage_root"]):
+        yield app
+
+
+@pytest.yield_fixture(scope="function")
+async def prepared_app(app):
     runner = web.AppRunner(app)
-
-    cwd = Path(os.path.dirname(__file__))
-    sql_root = cwd / ".." / "src" / "wallet" / "storage" / "sql"
-
-    cmd = "cat {schema} | PGPASSWORD='{password}' psql -h {host} -p {port} -d {database} -U {user}"  # noqa
-
-    subprocess.call(
-        [
-            cmd.format(
-                schema=(sql_root / "upgrade_schema.sql").as_posix(),
-                host=config.db.host,
-                port=config.db.port,
-                user=config.db.user,
-                password=config.db.password,
-                database=config.db.database,
-            )
-        ],
-        shell=True,
-        cwd=cwd.as_posix(),
-    )
-
-    loop.run_until_complete(runner.setup())
+    await runner.setup()
 
     yield app
 
-    loop.run_until_complete(runner.cleanup())
-
-    subprocess.call(
-        [
-            cmd.format(
-                schema=(sql_root / "downgrade_schema.sql").as_posix(),
-                host=config.db.host,
-                port=config.db.port,
-                user=config.db.user,
-                password=config.db.password,
-                database=config.db.database,
-            )
-        ],
-        shell=True,
-        cwd=cwd.as_posix(),
-    )
+    await runner.cleanup()
 
 
-@pytest.fixture(scope="session")
-def fake():
-    return faker.Faker()
+@pytest.fixture(scope="function")
+async def client(app, aiohttp_client):
+    client = await aiohttp_client(app)
+
+    return client
 
 
 @pytest.fixture(scope="session")
@@ -104,8 +66,3 @@ def today():
 @pytest.fixture(scope="session")
 def month(today):
     return today.start_of("month").date()
-
-
-@pytest.fixture(scope="function")
-def user(fake: Any) -> User:
-    return User(key=1, email=fake.free_email())
